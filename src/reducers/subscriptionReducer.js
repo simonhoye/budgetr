d31b0b4d9e9bff889eb3992c73475d2e953cce1c
Simon Hoye <simonhoye@gmail.com>
2017-05-06 21:03:55 +1000

    Fixed type error on reducerissue

--- src/reducers/subscriptionReducer.js
+++ src/reducers/subscriptionReducer.js
@@ -1,6 +1,6 @@
 import * as types from '../actions/actionTypes';
 
-export default function subscriptionReducer(state = {}, action) {
+export default function subscriptionReducer(state = [], action) {
   switch(action.type) {
     case types.LOAD_SUBSCRIPTIONS_SUCCESS:
       return action.subscriptions;
