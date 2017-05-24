4dd350bfdded17b9deb545f8cc966e73388f4c4e
Simon Hoye <simonhoye@gmail.com>
2017-05-24 19:29:13 +1000

    Added firebase and common components

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import firebase from 'firebase';
 import { ActionCreators } from '../actions';
 import AppWithNavigationState from '../navigators/AppNavigator';
 
@@ -12,6 +13,16 @@ import {
 class AppContainer extends Component {
     constructor(props) {
         super(props);
+    }
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDRBzhtdZ_5OQ31YOhHPbsOIcqpTecHws0",
+            authDomain: "budgetr-b3b4d.firebaseapp.com",
+            databaseURL: "https://budgetr-b3b4d.firebaseio.com",
+            projectId: "budgetr-b3b4d",
+            storageBucket: "budgetr-b3b4d.appspot.com",
+            messagingSenderId: "572275492321"
+        });
     }
     render() {
         return (
